5e154b746776925a3a7923f1f01ccc689b5ea6e6
TakedaRyota <tkdryt05@gmail.com>
2025-11-23 16:13:55 +0900

    成功ページ

--- src/app/success/page.tsx
+++ src/app/success/page.tsx
@@ -1,8 +1,7 @@
+import { SingleViewLayout } from '@/layouts/SingleViewLayout'
+import { Header } from '@/views/header'
+import { SuccessView } from '@/views/success'
+
 export default function Success() {
-  return (
-    <div className="p-6">
-      <h1 className="text-xl font-bold mb-4 text-green-600">購入が完了しました！</h1>
-      <p>ご購入ありがとうございます！</p>
-    </div>
-  )
+  return <SingleViewLayout header={<Header />} allView={<SuccessView />} />
 }
