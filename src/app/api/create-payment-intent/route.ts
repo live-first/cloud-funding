0674a9849b89ff8d5d5573c3c4b7492d81156a77
TakedaRyota <tkdryt05@gmail.com>
2025-12-13 13:42:10 +0900

    銀行振込を有効に

--- src/app/api/create-payment-intent/route.ts
+++ src/app/api/create-payment-intent/route.ts
@@ -7,16 +7,34 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
 
 export async function POST(req: Request) {
   try {
-    const { amount } = await req.json()
+    const { amount, email, name } = await req.json()
 
     if (!amount || amount <= 0) {
       return NextResponse.json({ error: '金額が不正です' }, { status: 400 })
     }
 
+    const customer = await stripe.customers.create({
+      email: email,
+      name: name,
+    })
+
+    console.log(`email: ${email}, name: ${name}`)
+
+    const customerId = customer.id
+
     const paymentIntent = await stripe.paymentIntents.create({
       amount: amount,
       currency: 'jpy',
-      automatic_payment_methods: { enabled: true },
+      payment_method_types: ['card', 'customer_balance'],
+      payment_method_options: {
+        customer_balance: {
+          funding_type: 'bank_transfer',
+          bank_transfer: {
+            type: 'jp_bank_transfer',
+          },
+        },
+      },
+      customer: customerId,
     })
 
     return NextResponse.json({ clientSecret: paymentIntent.client_secret })
